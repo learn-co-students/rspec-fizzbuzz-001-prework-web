7af83c6158819f1101878dab1aee31f1bca1e2c8
flats <dave@robador.com>
2015-12-01 12:06:01 -0500

    alternate solution

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,22 @@
-def fizzbuzz(number)
-  if (number % 3 == 0) && (number % 5 == 0)
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else
-    nil
-  end
+require 'pry'
+
+# def fizzbuzz(number)
+#   if (number % 3 == 0) && (number % 5 == 0)
+#     "FizzBuzz"
+#   elsif number % 3 == 0
+#     "Fizz"
+#   elsif number % 5 == 0
+#     "Buzz"
+#   else
+#     nil
+#   end
+# end
+
+def fizzbuzz(num)
+  result = ""
+  result += "Fizz" if (num % 3).zero?
+  result += "Buzz" if (num % 5).zero?
+  # result == "" ? nil : result
+  return nil if result.empty?
+  result
 end
