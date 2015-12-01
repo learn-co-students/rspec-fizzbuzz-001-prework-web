31f3dbe3e81d1b84753988d3d36b5d55082c5fa3
flats <dave@robador.com>
2015-12-01 11:31:09 -0500

    eliminate unnecessary returns

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
-    return "FizzBuzz"
+    "FizzBuzz"
   elsif number % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif number % 5 == 0
-    return "Buzz"
+    "Buzz"
   else
-    return nil
+    nil
   end
 end
