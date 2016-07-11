e00f6f07b49fae84589d5afef2c08c869b8475ca
Janelle Rosario <janelle.rosario@gmail.com>
2016-07-11 04:57:40 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz (int)
-  if int % 15 == 0
+def fizzbuzz (num)
+  if num % 15 == 0
     return "FizzBuzz"
-  elsif int % 3 == 0
+  elsif num % 3 == 0
     return "Fizz"
-  elsif int % 5 == 0
+  elsif num % 5 == 0
     return "Buzz"
   else
     return nil
