741de4aa53b82db73cfc419b46626310f6e27502
Jose Marin <josemarinemail@gmail.com>
2016-07-25 18:50:13 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(int)
+if int % 15 == 0
+  "FizzBuzz"
+elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 15 == 0
+  "FizzBuzz"
+  end
+end
+fizzbuzz(15)
+fizzbuzz(3) 
+fizzbuzz(5)
+fizzbuzz(15)
