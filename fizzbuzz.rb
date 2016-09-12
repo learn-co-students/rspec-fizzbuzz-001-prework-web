6dddd06f6b33a05601bdab322e4aaa56ca4512b4
Leah Einhorn <leinhorn@broadfinancial.com>
2016-09-12 11:41:54 -0400

    refactor fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,5 @@
 def fizzbuzz(number)
-  if number % 15 ==  0
-    return "FizzBuzz"
-  elsif number % 5 == 0
-    return "Buzz"
-  elsif number % 3  == 0
-    return "Fizz"
-  else 
-    puts
-  end
+  return "FizzBuzz" if number % 15 == 0
+  return "Fizz" if number % 3 == 0
+  return "Buzz" if number % 5 == 0
 end
